bb3a58a99e5a1f06dc13ca0241d7367c109f94d6
Shafayet098 <shafayethossenchowdhury@gmail.com>
2026-03-14 13:53:28 +0600

    completed-setup-packages

---
+++ src/Pages/Root/Root.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from '../../Components/Nav/Nav';
+
+const Root = () => {
+    return (
+        <div>
+            <div className='container mx-auto'>
+                <Nav></Nav>
+            </div>
+        </div>
+    );
+};
+
+export default Root;
